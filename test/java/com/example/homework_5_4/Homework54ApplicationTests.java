0069b86bbb971997747744414664d736bf55a8c8
Denis <demaz@bk.ru>
2023-01-24 20:01:23 +0700

    initial commit

---
+++ test/java/com/example/homework_5_4/Homework54ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework_5_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework54ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
